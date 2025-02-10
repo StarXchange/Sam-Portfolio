bf9520a8b169db7e076914a749cce2c93a973821
StarXchange <sammygeorge385@gmail.com>
2025-02-10 20:48:56 +0100

    major changes

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import { defineConfig } from 'vite'
  import autoprefixer from 'autoprefixer';
 
 
-// https://vite.dev/config/
+
 export default defineConfig({
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust paths to match your project structure
   ],
    plugins: [
-     react(),
+     react(
+      
+     ),
      tailwindcss(),
      autoprefixer(),
    ],
